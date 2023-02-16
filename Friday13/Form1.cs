fa1f231bc0451b242c0b64eed953deafb4889812
11999911.93@mail.ru <11999911.93@mail.ru>
2023-02-16 10:37:55 +0300

    Добавьте файлы проекта.

---
+++ Friday13/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Friday13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime friday = new DateTime(Convert.ToInt32(textBox1.Text), 1, 13);
+            List<string> list = new List<string>();
+        }
+    }
+}
